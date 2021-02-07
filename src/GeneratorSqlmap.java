56498e28a46a5842d460c38c7d077a891dafe773
liubiao_glb <liubiao0626@163.com>
2021-02-08 01:06:24 +0800

    添加备注

--- src/GeneratorSqlmap.java
+++ src/GeneratorSqlmap.java
@@ -14,7 +14,7 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 public class GeneratorSqlmap {
 
 	public void generator() throws Exception{
-
+		// 添加备注
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
